75ddaefd94e6e9c3de3ee432b25422d9450fdc5a
Marcus Twichel <marc.twichy@gmail.com>
2019-07-07 23:44:50 -0600

    implemented deleteCustomer and deleteCustomerCard

--- lib/src/customer/customer.dart
+++ lib/src/customer/customer.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:square_connect/square_connect.dart';
+
+import '../helper-classes.dart';
 
 class CustomerApi {
   final String token;
@@ -10,7 +14,7 @@ class CustomerApi {
       : assert(token != null),
         assert(client != null);
 
-  CreateCustomerResponse createCustomer({
+  Future<CreateCustomerResponse> createCustomer({
     String givenName,
     String familyName,
     String companyName,
@@ -21,42 +25,61 @@ class CustomerApi {
     String note,
     DateTime birthday,
     Address address,
-  }) {
+  }) async{
     
   }
 
-  CreateCustomerCardResponse createCustomerCard({
+  Future<CreateCustomerCardResponse> createCustomerCard({
     String cardNonce,
     Address billingAddress,
     String cardholderName,
     String verificationToken,
-  }) {
+  }) async{
 
   }
 
-  DeleteCustomerResponse deleteCustomer({String customerId}){
+  Future<DeleteCustomerResponse> deleteCustomer({@required String customerId}) async{
+    if (customerId == null)  throw ArgumentError('customerId must not be null');
+    var obj = RequestObj(
+      token: token,
+      path: '/v2/customers/$customerId',
+      method: RequestMethod.delete,
+      client: client,
+    );
 
+    var response = await obj.makeCall();
+    return DeleteCustomerResponse.fromJson(json.decode(response.body));
   }
 
-  DeleteCustomerCardResponse deleteCustomerCard({String customerId, String cardId}){
+  Future<DeleteCustomerCardResponse> deleteCustomerCard({String customerId, String cardId}) async{
+    if (customerId == null)  throw ArgumentError('customerId must not be null');
+    if (cardId == null)  throw ArgumentError('cardId must not be null');
+    var obj = RequestObj(
+      token: token,
+      path: '/v2/customers/$customerId/cards/$cardId',
+      method: RequestMethod.delete,
+      client: client,
+    );
 
+    var response = await obj.makeCall();
+    return DeleteCustomerCardResponse.fromJson(json.decode(response.body));
   }
 
-  ListCustomersResponse listCustomers({
+  Future<ListCustomersResponse> listCustomers({
     String cursor,
     String sortField,
     SortOrder sortOrder,
-  }){
+  })async{
 
   }
 
-  RetrieveCustomerResponse retrieveCustomer({
+  Future<RetrieveCustomerResponse> retrieveCustomer({
     String customerId,
   }){
 
   }
 
-  SearchCustomersResponse searchCustomers({
+  Future<SearchCustomersResponse> searchCustomers({
     String cursor,
     int limit,
     CustomerCreationSource source,
@@ -66,11 +89,11 @@ class CustomerApi {
     DateTime updatedAtEnd,
     CustomerSortField sortField,
     SortOrder sortOrder,
-  }){
+  })async{
 
   }
 
-  UpdateCustomerResponse updateCustomer({
+  Future<UpdateCustomerResponse> updateCustomer({
     @required String customerId,
     String givenName,
     String familyName,
@@ -82,7 +105,7 @@ class CustomerApi {
     String referenceId,
     String note,
     DateTime birthday,
-  }){
+  })async{
 
   }
 }

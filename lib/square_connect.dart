3c07d8ce09047ebf61d6c068564b0a93896d46f5
Marcus Twichel <marc.twichy@gmail.com>
2019-07-17 17:21:10 -0600

    made API objects part of global scope

--- lib/square_connect.dart
+++ lib/square_connect.dart
@@ -9,18 +9,25 @@ import 'package:square_connect/src/orders/orders.dart';
 export 'package:square_connect/src/catalog/catalog-return-objects.dart';
 export 'package:square_connect/src/catalog/catalog-objects.dart';
 export 'package:square_connect/src/catalog/catalog-enums.dart';
+export 'package:square_connect/src/catalog/catalog.dart';
+
 
 export 'package:square_connect/src/customer/customer-return-objects.dart';
 export 'package:square_connect/src/customer/customer-objects.dart';
 export 'package:square_connect/src/customer/customer-enums.dart';
+export 'package:square_connect/src/customer/customer.dart';
+
 
 export 'package:square_connect/src/locations/locations-return-objects.dart';
 export 'package:square_connect/src/locations/locations-objects.dart';
 export 'package:square_connect/src/locations/locations-enums.dart';
+export 'package:square_connect/src/locations/locations.dart';
+
 
 export 'package:square_connect/src/orders/orders-return-objects.dart';
 export 'package:square_connect/src/orders/orders-objects.dart';
 export 'package:square_connect/src/orders/orders-enums.dart';
+export 'package:square_connect/src/orders/orders.dart';
 
 export 'package:square_connect/src/errors.dart';
 export 'package:square_connect/src/money.dart';

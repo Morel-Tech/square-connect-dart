dddcafa99b75af6e444dc21792bcc2d86804f1fa
Marcus Twichel <marc.twichy@gmail.com>
2019-07-17 20:37:35 -0600

    Refactoring locations enums

--- lib/src/locations/locations-enums.dart
+++ lib/src/locations/locations-enums.dart
@@ -1,18 +1,21 @@
+/// Indicates payment capabilities that a location might or might not have enabled.
 enum LocationCapability {
   creditCardProcessing,
 }
 
-///
+/// Indicates if a location is active or inactive.
 enum LocationStatus {
   active,
   inactive,
 }
 
+/// Indicates a type of location (physical or mobile).
 enum LocationType {
   physical,
   mobile,
 }
 
+/// Indicates a day of a 7 day week.
 enum DayOfWeek {
   sunday,
   monday,

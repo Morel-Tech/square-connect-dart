64661cc2fe6c74ad1a6424bea4383a60b28de55e
Marcus Twichel <marc.twichy@gmail.com>
2019-07-19 00:40:00 -0600

    More begining work to implemement transactions api

--- lib/src/transactions/transactions-return-objects.dart
+++ lib/src/transactions/transactions-return-objects.dart
@@ -53,3 +53,159 @@ class ChargeResponse {
     return errors != null;
   }
 }
+
+/// The response to the `createRefund()` method in [TransactionsApi].
+class CreateRefundResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The newly created [Refund].
+  final Refund refund;
+
+  CreateRefundResponse({
+    this.errors,
+    this.refund,
+  });
+
+  factory CreateRefundResponse.fromJson(Map<String, dynamic> json) {
+    return CreateRefundResponse(
+        errors: json['errors'] != null
+            ? (json['errors'] as List)
+                .map((error) => SquareError.fromJson(error))
+                .toList()
+            : null,
+        refund: json['refund'] != null
+            ? Refund.fromJson(json['refund'])
+            : null);
+  }
+
+  get hasErrors {
+    return errors != null;
+  }
+}
+
+/// The response to the `listRefunds()` method in [TransactionsApi].
+class ListRefundsResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The returned [Refund]s.
+  final List<Refund> refunds;
+
+  /// A pagination cursor to be used in a future call.
+  final String cursor;
+
+  ListRefundsResponse({
+    this.errors,
+    this.refunds,
+    this.cursor,
+  });
+
+  factory ListRefundsResponse.fromJson(Map<String, dynamic> json) {
+    return ListRefundsResponse(
+        errors: json['errors'] != null
+            ? (json['errors'] as List)
+                .map((error) => SquareError.fromJson(error))
+                .toList()
+            : null,
+        refunds: json['refund'] != null
+            ? (json['refund'] as List).map((item) => Refund.fromJson(item)).toList()
+            : null,
+        cursor: json['cursor']
+    );
+  }
+
+  get hasErrors {
+    return errors != null;
+  }
+}
+
+/// The response to the `listTransactions()` method in [TransactionsApi].
+class ListTransactionsResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The returned [Transaction=]s.
+  final List<Transaction> transactions;
+
+  /// A pagination cursor to be used in a future call.
+  final String cursor;
+
+  ListTransactionsResponse({
+    this.errors,
+    this.transactions,
+    this.cursor,
+  });
+
+  factory ListTransactionsResponse.fromJson(Map<String, dynamic> json) {
+    return ListTransactionsResponse(
+        errors: json['errors'] != null
+            ? (json['errors'] as List)
+                .map((error) => SquareError.fromJson(error))
+                .toList()
+            : null,
+        transactions: json['transactions'] != null
+            ? (json['transactions'] as List).map((item) => Transaction.fromJson(item)).toList()
+            : null,
+        cursor: json['cursor']
+    );
+  }
+
+  get hasErrors {
+    return errors != null;
+  }
+}
+
+/// The response to the `retrieveTransaction()` method in [TransactionsApi].
+class RetrieveTransactionResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved transaction.
+  final Transaction transaction;
+
+  RetrieveTransactionResponse({
+    this.errors,
+    this.transaction,
+  });
+
+  factory RetrieveTransactionResponse.fromJson(Map<String, dynamic> json) {
+    return RetrieveTransactionResponse(
+        errors: json['errors'] != null
+            ? (json['errors'] as List)
+                .map((error) => SquareError.fromJson(error))
+                .toList()
+            : null,
+        transaction: json['transaction'] != null
+            ? Transaction.fromJson(json['transaction'])
+            : null);
+  }
+
+  get hasErrors {
+    return errors != null;
+  }
+}
+
+/// The response to the `voidTransaction()` method in [TransactionsApi].
+class VoidTransactionResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  VoidTransactionResponse({
+    this.errors,
+  });
+
+  factory VoidTransactionResponse.fromJson(Map<String, dynamic> json) {
+    return VoidTransactionResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+    );
+  }
+
+  get hasErrors {
+    return errors != null;
+  }
+}
--- lib/src/transactions/transactions.dart
+++ lib/src/transactions/transactions.dart
@@ -1,1 +1,82 @@
+import 'package:http/http.dart';
+import 'package:square_connect/square_connect.dart';
 
+/// An object containing all methods for the Transactions API. Retrieve this object with `SquareConnect.instance.transactionsApi`.
+class TransactionsApi {
+  final String _token;
+  final Client _client;
+
+  TransactionsApi(this._token, this._client);
+
+  Future<CaptureTransactionResponse> captureTransaction({
+    String locationId,
+    String transactionId,
+  }) async {
+
+  }
+
+  Future<ChargeResponse> charge({
+    String locationId,
+    String idempotencyKey,
+    String cardNonce,
+    String customerCardId,
+    bool delayCapture,
+    String referenceId,
+    String note,
+    String customerId,
+    Address billingAddress,
+    Address shippingAddress,
+    String buyerEmailAddress,
+    String orderId,
+    List<AdditionalRecipient> additionalRecipients,
+    String verificationToken,
+  }) async {
+
+  }
+
+  Future<CreateRefundResponse> createRefund({
+    String locationId,
+    String transactionId,
+    String idempotencyKey,
+    String tenderId,
+    String reason,
+    Money amountMoney,
+  }) async {
+
+  }
+
+    Future<ListRefundsResponse> listRefunds({
+    String locationId,
+    DateTime beginTime,
+    DateTime endTime,
+    SortOrder sortOrder,
+    String cursor,
+  }) async {
+
+  }
+
+  Future<ListTransactionsResponse> listTransactions({
+    String locationId,
+    DateTime beginTime,
+    DateTime endTime,
+    SortOrder sortOrder,
+    String cursor,
+  }) async {
+
+  }
+
+  Future<RetrieveTransactionResponse> retrieveTransaction({
+    String locationId,
+    String transactionId,
+  }) async {
+
+  }
+
+  Future<VoidTransactionResponse> voidTransaction({
+    String locationId,
+    String transactionId,
+  }) async {
+
+  }
+
+}

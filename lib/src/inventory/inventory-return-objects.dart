6bba333959c3dc01324da839b46b88afda4bc058
Marcus Twichel <marc.twichy@gmail.com>
2019-08-06 19:25:39 -0600

    Add response objects for inventory api

--- lib/src/inventory/inventory-return-objects.dart
+++ lib/src/inventory/inventory-return-objects.dart
@@ -55,12 +55,173 @@ class BatchRetrieveInventoryChangesResponse extends ApiResponse {
               .map((error) => SquareError.fromJson(error))
               .toList()
           : null,
-      changes: json['counts'] != null
-          ? (json['counts'] as List)
+      changes: json['changes'] != null
+          ? (json['changes'] as List)
               .map((error) => InventoryChange.fromJson(error))
               .toList()
           : null,
       cursor: json['cursor'],
+    );
+  }
+}
+
+/// The response to the `batchRetrieveInventoryCounts()` method in [InventoryApi].
+class BatchRetrieveInventoryCountsResponse extends ApiResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved [InventoryCount]s.
+  final List<InventoryCount> counts;
+
+  /// A pagination cursor to be used in a future call.
+  final String cursor;
+
+  BatchRetrieveInventoryCountsResponse({
+    this.errors,
+    this.counts,
+    this.cursor,
+  });
+
+  factory BatchRetrieveInventoryCountsResponse.fromJson(
+      Map<String, dynamic> json) {
+    return BatchRetrieveInventoryCountsResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+      counts: json['counts'] != null
+          ? (json['counts'] as List)
+              .map((error) => InventoryCount.fromJson(error))
+              .toList()
+          : null,
+      cursor: json['cursor'],
+    );
+  }
+}
+
+/// The response to the `retrieveInventoryAdjustment()` method in [InventoryApi].
+class RetrieveInventoryAdjustmentResponse extends ApiResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved [InventoryAdjustment].
+  final InventoryAdjustment adjustment;
+
+  RetrieveInventoryAdjustmentResponse({
+    this.errors,
+    this.adjustment,
+  });
+
+  factory RetrieveInventoryAdjustmentResponse.fromJson(
+      Map<String, dynamic> json) {
+    return RetrieveInventoryAdjustmentResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+      adjustment: json['adjustment'] != null
+          ? InventoryAdjustment.fromJson(json['adjustment'])
+          : null,
+    );
+  }
+}
+
+/// The response to the `retrieveInventoryChangesChanges()` method in [InventoryApi].
+class RetrieveInventoryChangesResponse extends ApiResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved [InventoryChange]s.
+  final List<InventoryChange> changes;
+
+  /// A pagination cursor to be used in a future call.
+  final String cursor;
+
+  RetrieveInventoryChangesResponse({
+    this.errors,
+    this.changes,
+    this.cursor,
+  });
+
+  factory RetrieveInventoryChangesResponse.fromJson(
+      Map<String, dynamic> json) {
+    return RetrieveInventoryChangesResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+      changes: json['changes'] != null
+          ? (json['changes'] as List)
+              .map((error) => InventoryChange.fromJson(error))
+              .toList()
+          : null,
+      cursor: json['cursor'],
+    );
+  }
+}
+
+/// The response to the `retrieveInventoryCount()` method in [InventoryApi].
+class RetrieveInventoryCountResponse extends ApiResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved [InventoryCount]s.
+  final List<InventoryCount> counts;
+
+  /// A pagination cursor to be used in a future call.
+  final String cursor;
+
+  RetrieveInventoryCountResponse({
+    this.errors,
+    this.counts,
+    this.cursor,
+  });
+
+  factory RetrieveInventoryCountResponse.fromJson(
+      Map<String, dynamic> json) {
+    return RetrieveInventoryCountResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+      counts: json['counts'] != null
+          ? (json['counts'] as List)
+              .map((error) => InventoryCount.fromJson(error))
+              .toList()
+          : null,
+      cursor: json['cursor'],
+    );
+  }
+}
+
+/// The response to the `retrieveInventoryPhysicalCount()` method in [InventoryApi].
+class RetrieveInventoryPhysicalCountResponse extends ApiResponse {
+  /// The list of errors if any exist.
+  final List<SquareError> errors;
+
+  /// The retrieved [InventoryCount]s.
+  final InventoryPhysicalCount count;
+
+  RetrieveInventoryPhysicalCountResponse({
+    this.errors,
+    this.count,
+  });
+
+  factory RetrieveInventoryPhysicalCountResponse.fromJson(
+      Map<String, dynamic> json) {
+    return RetrieveInventoryPhysicalCountResponse(
+      errors: json['errors'] != null
+          ? (json['errors'] as List)
+              .map((error) => SquareError.fromJson(error))
+              .toList()
+          : null,
+      count: json['count'] != null
+          ? InventoryPhysicalCount.fromJson(json['count'])
+          : null,
     );
   }
 }

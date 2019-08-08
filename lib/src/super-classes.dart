38475e2850f099968690bc698fa5137e98863b54
Marcus Twichel <marc.twichy@gmail.com>
2019-08-08 11:34:38 -0600

    hotfix 3

--- lib/src/super-classes.dart
+++ lib/src/super-classes.dart
@@ -3,5 +3,5 @@ import 'package:square_connect/square_connect.dart';
 abstract class ApiResponse {
   List<SquareError> get errors;
 
-  bool get hasErrors => this.errors != null && this.errors.length<0;
+  bool get hasErrors => this.errors != null;
 }

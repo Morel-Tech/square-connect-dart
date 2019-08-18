62d4d9c71ce934a2ba8143fd428db4bffe2a85cb
Marcus Twichel <marc.twichy@gmail.com>
2019-08-17 21:48:44 -0600

    added convesion from inapp payments card to connect card

--- lib/src/customer/customer-objects.dart
+++ lib/src/customer/customer-objects.dart
@@ -1,5 +1,6 @@
 import 'package:square_connect/src/customer/customer-enums-converter.dart';
 import 'package:square_connect/square_connect.dart';
+import 'package:square_in_app_payments/models.dart' as inapp;
 
 /// An object representing a customer identity. The Customer's id field is used throught the apis.
 class Customer {
@@ -207,6 +208,16 @@ class Card {
     if (fingerprint != null) body['fingerprint'] = fingerprint;
 
     return body;
+  }
+
+  factory Card.fromInAppPaymentsCard(inapp.Card card) {
+    return Card(
+      last4: card.lastFourDigits,
+      expMonth: card.expirationMonth,
+      expYear: card.expirationYear,
+      cardBrand: getCardBrandFromInAppCardBrand(card.brand),
+      billingAddress: Address(postalCode: card.postalCode),
+    );
   }
 }
 

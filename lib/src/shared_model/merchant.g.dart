f83feab1f55620ccadfd19898b640cc5152441d2
BruceClark406 <brucewestonmt@gmail.com>
2023-02-14 15:44:29 -0700

    fix: merchant fields not required (#96)

--- lib/src/shared_model/merchant.g.dart
+++ lib/src/shared_model/merchant.g.dart
@@ -7,21 +7,30 @@ part of 'merchant.dart';
 // **************************************************************************
 
 Merchant _$MerchantFromJson(Map<String, dynamic> json) => Merchant(
-      id: json['id'] as String,
+      id: json['id'] as String?,
       country: json['country'] as String,
-      businessName: json['business_name'] as String,
-      currency: json['currency'] as String,
-      languageCode: json['language_code'] as String,
-      mainLocationId: json['main_location_id'] as String,
-      status: json['status'] as String,
+      businessName: json['business_name'] as String?,
+      currency: json['currency'] as String?,
+      languageCode: json['language_code'] as String?,
+      mainLocationId: json['main_location_id'] as String?,
+      status: json['status'] as String?,
     );
 
-Map<String, dynamic> _$MerchantToJson(Merchant instance) => <String, dynamic>{
-      'id': instance.id,
-      'country': instance.country,
-      'business_name': instance.businessName,
-      'currency': instance.currency,
-      'language_code': instance.languageCode,
-      'main_location_id': instance.mainLocationId,
-      'status': instance.status,
-    };
+Map<String, dynamic> _$MerchantToJson(Merchant instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  val['country'] = instance.country;
+  writeNotNull('business_name', instance.businessName);
+  writeNotNull('currency', instance.currency);
+  writeNotNull('language_code', instance.languageCode);
+  writeNotNull('main_location_id', instance.mainLocationId);
+  writeNotNull('status', instance.status);
+  return val;
+}
